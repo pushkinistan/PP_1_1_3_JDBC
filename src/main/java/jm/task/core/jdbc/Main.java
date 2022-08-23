9a4db0abd4e5eaabaf25d089e7838f4f1e4159d4
Бойченко Вадим Валерьевич <vadim.boychenko@gmail.com>
2022-08-23 12:06:32 +0300

    final after mentor's comment

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,25 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDaoJDBCImpl();
+        //UserDao userDao = new UserDaoJDBCImpl();
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userServiceImpl.createUsersTable();
 
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        userServiceImpl.saveUser("Name1", "LastName1", (byte) 20);
+        userServiceImpl.saveUser("Name2", "LastName2", (byte) 25);
+        userServiceImpl.saveUser("Name3", "LastName3", (byte) 31);
+        userServiceImpl.saveUser("Name4", "LastName4", (byte) 38);
 
-        userDao.removeUserById(1);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userServiceImpl.removeUserById(1);
+        userServiceImpl.getAllUsers();
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
 
